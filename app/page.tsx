491c6783ecb9c28ea095b03497e223482b29d8a4
Idelia Muthia <ideliamuthia@gmail.com>
2025-10-06 14:47:23 +0700

    add initial page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-600">
+        Pink Pocket Journal 🎀
+      </h1>
+    </main>
+  );
+}
