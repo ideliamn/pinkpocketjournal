2dda85980b49e0a2108ff6f32fb93900765c5e85
Idelia Muthia <ideliamuthia@gmail.com>
2025-10-08 17:00:52 +0700

    change font & favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Pixelify_Sans } from "next/font/google";
+
+const pixelify = Pixelify_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-pink-100">
-      <h1 className="text-4xl font-bold text-pink-600">
+      <h1 className={`flex h-screen items-center justify-center ${pixelify.className} text-xl`}>
         Pink Pocket Journal 🎀
       </h1>
     </main>
