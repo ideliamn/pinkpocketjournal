954aa1736cc1863055df822a12dcf6d80836e69e
Idelia Muthia <ideliamuthia@gmail.com>
2025-10-10 15:12:19 +0700

    update user dropdown

--- app/layout/AppHeader.tsx
+++ app/layout/AppHeader.tsx
@@ -3,9 +3,9 @@ import UserDropdown from "../components/header/UserDropdown";
 
 const AppHeader: React.FC = () => {
     return (
-        <header className="top-0 w-full bg-white">
-            <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
-                <div className="flex items-center w-full gap-4 px-5 py-4 lg:flex shadow-theme-md justify-end lg:px-0 lg:shadow-none">
+        <header className="top-0 w-full">
+            <div className="flex flex-col items-center justify-between grow">
+                <div className="flex items-center w-full gap-4 shadow-theme-md justify-end">
                     <UserDropdown />
                 </div>
             </div>
