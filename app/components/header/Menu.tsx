eae572ed133be64046a7649dd2ca69b6fafa9c35
Idelia Muthia <ideliamuthia@gmail.com>
2025-11-05 14:13:50 +0700

    set loading when open another menu

--- app/components/header/Menu.tsx
+++ app/components/header/Menu.tsx
@@ -2,44 +2,55 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Pixelify_Sans } from "next/font/google";
+import Loading from "../common/Loading";
+import { usePathname } from "next/navigation";
 
 const pixelify = Pixelify_Sans({
     subsets: ["latin"],
     weight: ["400"],
 });
+
 export default function Menu() {
     interface Menu {
         name: string;
         url: string;
     }
 
-    const [menu, setMenu] = useState<Menu[]>([])
+    const [menu, setMenu] = useState<Menu[]>([]);
+    const [loading, setLoading] = useState(false);
+    const pathname = usePathname();
 
     useEffect(() => {
-        getMenu()
-    }, [])
+        getMenu();
+    }, []);
+
+    useEffect(() => {
+        setLoading(false);
+    }, [pathname]);
 
     const getMenu = async () => {
         try {
-            const res = await fetch("/api/menu", {
-                method: "GET"
-            });
+            const res = await fetch("/api/menu");
             const data = await res.json();
-            if (res.ok) {
-                setMenu(data.data)
-            }
+            if (res.ok) setMenu(data.data);
         } catch (err) {
             console.error(err);
-        } finally {
         }
-    }
+    };
 
     return (
         <div className={`${pixelify.className} flex gap-4`}>
+            {loading && <Loading />}
             {menu.map((m) => (
-                <Link href={`${m.url}`} key={m.url} className="cursor-pointer hover:text-pink-600 hover:underline whitespace-nowrap">{m.name}</Link>
+                <Link
+                    href={m.url}
+                    key={m.url}
+                    onClick={() => setLoading(true)}
+                    className="cursor-pointer hover:text-pink-600 hover:underline whitespace-nowrap"
+                >
+                    {m.name}
+                </Link>
             ))}
-        </div >
+        </div>
     );
 }
-
